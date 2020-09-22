91d032aa37b3a7384e768c334ee5296c056ffeef
Daniel Cifuentes <danixbox1313@gmail.com>
2020-09-22 18:47:13 -0500

    added CategoriaFilter

--- src/main/java/equipo/rocket/headhunterbackend/model/FilterChain.java
+++ src/main/java/equipo/rocket/headhunterbackend/model/FilterChain.java
@@ -1,10 +1,11 @@
 package equipo.rocket.headhunterbackend.model;
 
 import java.util.HashMap;
+import java.util.List;
 
 public interface FilterChain {
     
     public void setNextFilter(FilterChain filter);
 
-    public void filter(Idea[] ideas,HashMap<String,Object> extraParams);
+    public void filter(List<Idea> ideas,HashMap<String,Object> extraParams);
 }
--- src/main/java/equipo/rocket/headhunterbackend/model/filters/CategoriaFilter.java
+++ src/main/java/equipo/rocket/headhunterbackend/model/filters/CategoriaFilter.java
@@ -1,21 +1,33 @@
 package equipo.rocket.headhunterbackend.model.filters;
 
 import java.util.HashMap;
+import java.util.List;
 
 import equipo.rocket.headhunterbackend.model.FilterChain;
 import equipo.rocket.headhunterbackend.model.Idea;
 
 public class CategoriaFilter implements FilterChain {
 
+    private FilterChain nextFilter;
+
     @Override
     public void setNextFilter(FilterChain filter) {
-        // TODO Auto-generated method stub
+        nextFilter=filter;
 
     }
 
     @Override
-    public void filter(Idea[] ideas, HashMap<String, Object> extraParams) {
-        // TODO Auto-generated method stub
+    public void filter(List<Idea> ideas, HashMap<String, Object> extraParams) {
+        if(extraParams.get("Categoria")!=null){//revisar que el tipo del parametro si concuerde para usarlo abajo
+            for (Idea idea : ideas) {
+                if(idea.getCategoria().equals(extraParams.get("Categoria"))){
+                    ideas.remove(idea);
+                }
+            }
+        }
+        if(nextFilter!=null){
+            nextFilter.filter(ideas, extraParams);
+        }
 
     }
     
