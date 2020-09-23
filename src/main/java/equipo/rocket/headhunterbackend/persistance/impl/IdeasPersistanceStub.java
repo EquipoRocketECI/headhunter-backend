248ad3278eb775dcf190634fce118de8a2d3f04e
Daniel Cifuentes <danixbox1313@gmail.com>
2020-09-23 17:25:18 -0500

    modified stub persistance to test with more interesting data

--- src/main/java/equipo/rocket/headhunterbackend/persistance/impl/IdeasPersistanceStub.java
+++ src/main/java/equipo/rocket/headhunterbackend/persistance/impl/IdeasPersistanceStub.java
@@ -16,10 +16,15 @@ public class IdeasPersistanceStub implements IdeasPersistance {
 
     public IdeasPersistanceStub() {
         ideas = new ArrayList<Idea>();
-        Idea idea1 = new Idea("nombre1", "descripcion1", new Date(), 10000, 1000, "categoria", 5, "imagen");
-        Idea idea2 = new Idea("nombre2", "descripcion2", new Date(), 10000, 1000, "categoria", 5, "imagen");
-        Idea idea3 = new Idea("nombre3", "descripcion3", new Date(), 10000, 1000, "categoria", 5, "imagen");
-        Idea idea4 = new Idea("nombre4", "descripcion4", new Date(), 10000, 1000, "categoria", 5, "imagen");
+        Idea idea1 = new Idea("HeadHunter",
+                "HeadHunter es una plataforma para emprendedores que buscan ayuda financiera y logística para llevar a cabo sus ideas a la realidad",
+                new Date(), 10000, 1000, "Educacion", 5, "imagen");
+        Idea idea2 = new Idea("MyDoctor", "Sistema para pedir consejos de médicos", new Date(), 1000000, 100000,
+                "Salud", 3, "imagen");
+        Idea idea3 = new Idea("BlueApron",
+                "Our food system—the way in which food is grown and distributed—is complicated, and making good choices for your family can be difficult. We are changing that: By partnering with farmers to raise the highest-quality ingredients, by creating a distribution system that delivers ingredients at a better value and by investing in the things that matter most—our environment and our communities",
+                new Date(), 2500000, 100000, "Gastronomia", 5, "imagen");
+        Idea idea4 = new Idea("PokemonGo", "juego de AR", new Date(), 50000, 10000, "Cultura", 5, "imagen");
 
         ideas.add(idea1);
         ideas.add(idea2);
@@ -29,9 +34,9 @@ public class IdeasPersistanceStub implements IdeasPersistance {
 
     @Override
     public List<Idea> getAllIdeas() {
-        System.out.println(ideas.toString()+"ideas!!!!!!!!!");
+        System.out.println(ideas.toString() + "ideas!!!!!!!!!");
         return ideas;
-        
+
     }
 
     @Override
@@ -39,5 +44,5 @@ public class IdeasPersistanceStub implements IdeasPersistance {
         // TODO Auto-generated method stub
         return null;
     }
-    
+
 }
