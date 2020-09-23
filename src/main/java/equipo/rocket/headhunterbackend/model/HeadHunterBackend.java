a909e90fa73d0999b740c6368bf886337cb92950
Daniel Cifuentes <danixbox1313@gmail.com>
2020-09-22 19:36:46 -0500

    added HeadHunterBackend class - equivalente a 'General.js' en frontend

---
+++ src/main/java/equipo/rocket/headhunterbackend/model/HeadHunterBackend.java
@@ -0,0 +1,11 @@
+package equipo.rocket.headhunterbackend.model;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class HeadHunterBackend {
+    
+    public void filter(List<Idea> ideas,HashMap<String,Object> extraParams){
+        
+    }
+}
