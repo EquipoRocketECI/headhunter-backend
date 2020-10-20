2885bf6d234ed0fe9ec849b7a756f2b7ec9e6813
Daniel Cifuentes <danixbox1313@gmail.com>
2020-10-20 17:17:43 -0500

    category filter can now filter by more than one category at the same time

--- src/main/java/equipo/rocket/headhunterbackend/services/filters/CategoriaFilter.java
+++ src/main/java/equipo/rocket/headhunterbackend/services/filters/CategoriaFilter.java
@@ -1,7 +1,9 @@
 package equipo.rocket.headhunterbackend.services.filters;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import equipo.rocket.headhunterbackend.model.Idea;
@@ -12,27 +14,37 @@ public class CategoriaFilter implements FilterChain {
 
     @Override
     public void setNextFilter(FilterChain filter) {
-        nextFilter=filter;
+        nextFilter = filter;
 
     }
 
     @Override
     public void filter(List<Idea> ideas, HashMap<String, Object> extraParams) {
-        //cambiar para que reciba varias categorias -> categoria:[<categorias seleccionadas>]
-        if(extraParams.containsKey("categoria")){
-            
-            Iterator<Idea> it = ideas.iterator();
-            while(it.hasNext()){
-                Idea idea = it.next();
-                if(!idea.getCategoria().equals(extraParams.get("categoria"))){
-                    it.remove();
+        // cambiar para que reciba varias categorias -> categoria:[<categorias
+        // seleccionadas>]
+        if (extraParams.containsKey("selectedCategories")) {
+            ArrayList<String> categories = new ArrayList<String>();
+
+            LinkedHashMap<String, Boolean> selectedCategories = ((LinkedHashMap<String, Boolean>) extraParams
+                    .get("selectedCategories"));
+            selectedCategories.forEach((k, v) -> {
+                if (v) {
+                    categories.add(k);
                 }
-            }
+            });
+            Iterator<Idea> it = ideas.iterator();
+                    while (it.hasNext()) {
+                        Idea idea = it.next();
+                        if (!categories.contains(idea.getCategoria())) {//compare not in set of true values
+                            it.remove();
+                        }
+                    }
+
         }
-        if(nextFilter!=null){
+        if (nextFilter != null) {
             nextFilter.filter(ideas, extraParams);
         }
 
     }
-    
+
 }
